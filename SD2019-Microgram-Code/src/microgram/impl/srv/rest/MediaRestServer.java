f694f6b971a080d0e5294afe53aab51246c39c58
Andre Atalaia <Andre Atalaia@10.22.108.232>
2019-04-09 14:40:12 +0100

    forgot classes , added now

---
+++ SD2019-Microgram-Code/src/microgram/impl/srv/rest/MediaRestServer.java
@@ -0,0 +1,40 @@
+package microgram.impl.srv.rest;
+
+import java.net.URI;
+import java.util.logging.Logger;
+
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import discovery.Discovery;
+import utils.IP;
+
+public class  MediaRestServer{
+	
+	private static Logger Log = Logger.getLogger(MediaRestServer.class.getName());
+
+	static {
+		System.setProperty("java.net.preferIPv4Stack", "true");
+		System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s\n");
+	}
+
+	public static final int PORT = 9999;
+	public static final String SERVICE = "Microgram-MediaStorage";
+	public static String SERVER_BASE_URI = "http://%s:%s/rest";
+
+	public static void main(String[] args) throws Exception {
+
+		String ip = IP.hostAddress();
+		String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
+
+		ResourceConfig config = new ResourceConfig();
+
+		config.register(new RestMediaResources(new URI(serverURI)));
+
+		JdkHttpServerFactory.createHttpServer(URI.create(serverURI.replace(ip, "0.0.0.0")), config);
+
+		Log.info(String.format("%s Server ready @ %s\n", SERVICE, serverURI));
+		
+		Discovery.announce(SERVICE, serverURI);
+	}
+}
