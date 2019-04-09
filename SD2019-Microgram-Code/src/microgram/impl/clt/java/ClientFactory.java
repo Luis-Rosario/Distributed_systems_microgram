a77a29c9e5dbbbaf7dcbcbb933e41d321e09efed
Luís Rosário <48826580+Luis-Rosario@users.noreply.github.com>
2019-04-09 15:08:49 +0100

    Client factoryDone

--- SD2019-Microgram-Code/src/microgram/impl/clt/java/ClientFactory.java
+++ SD2019-Microgram-Code/src/microgram/impl/clt/java/ClientFactory.java
@@ -2,21 +2,44 @@ package microgram.impl.clt.java;
 
 import java.net.URI;
 
-/*import impl.clt.rest.RestMediaClient;
-import impl.clt.soap.SoapMediaClient;*/
+import microgram.impl.clt.rest.RestMediaClient;
 import microgram.api.java.Media;
+import microgram.api.java.Posts;
+import microgram.api.java.Profiles;
+import microgram.impl.clt.rest.RestPostsClient;
+import microgram.impl.clt.rest.RestProfilesClient;
+import microgram.impl.clt.soap.SoapPostsClient;
+import microgram.impl.clt.soap.SoapProfilesClient;
 
 public class ClientFactory {
-/*
+
 	private static final String REST = "/rest";
 	private static final String SOAP = "/soap";
 
 	public static Media getMediaClient(URI uri) {
 		String uriString = uri.toString();
 		if (uriString.endsWith(REST))
-			return new RestMediaClient(uri);
-		else if (uriString.endsWith(SOAP))
-			return new SoapMediaClient(uri);
+			return new RetryMediaClient(new RestMediaClient(uri));
 		throw new RuntimeException("Unknown service type..." + uri);
-	}*/
+	}
+	
+	
+	public static Posts getPostsClient(URI uri) {
+		String uriString = uri.toString();
+		if (uriString.endsWith(REST))
+			return new RetryPostsClient(new RestPostsClient(uri));
+		else if (uriString.endsWith(SOAP))
+			return new RetryPostsClient(new SoapPostsClient(uri));
+		throw new RuntimeException("Unknown service type..." + uri);
+	}
+	
+	
+	public static Profiles getProfilesClient(URI uri) {
+		String uriString = uri.toString();
+		if (uriString.endsWith(REST))
+			return new RetryProfilesClient(new RestProfilesClient(uri));
+		else if (uriString.endsWith(SOAP))
+			return new RetryProfilesClient(new SoapProfilesClient(uri));
+		throw new RuntimeException("Unknown service type..." + uri);
+	}
 }
